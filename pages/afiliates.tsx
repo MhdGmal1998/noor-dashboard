8a85a555ac737ba052990b41f65203243ddc9427
SomayaAbdulkareem <90135545+MhdGmal1998@users.noreply.github.com>
2022-09-15 01:55:08 +0300

    upload noor dashboard

---
+++ pages/afiliates.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Layout from "../components/Layout";
+
+const Affiliate: NextPage = () => {
+    return (
+        <Layout>
+            <div>
+                all Affiliates
+            </div>
+        </Layout>
+    )
+}
+export default Affiliate
